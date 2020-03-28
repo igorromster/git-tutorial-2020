8f9baa50990ee8d90da67967ea007415fc2c27f2
Igor Romanov <igorromster@gmail.com>
2020-03-28 18:19:40 +0300

    Added a second runner

---
+++ src/main/java/com/romanov/Runner2.java
@@ -0,0 +1,10 @@
+package com.romanov;
+
+public class Runner2 {
+	
+	public static void main(String[] args) {
+		System.out.println(Runner2.class.getName() + " started");
+		System.out.println("Work in progress...");
+		System.out.println(Runner2.class.getName() + " finished");
+	}
+}
