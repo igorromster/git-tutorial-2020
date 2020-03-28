7e8a73aede98ed9658e7cf0c6d7c40f3dfc5e01c
Igor Romanov <igorromster@gmail.com>
2020-03-28 21:00:53 +0300

    55

--- src/main/java/com/romanov/Runner2.java
+++ src/main/java/com/romanov/Runner2.java
@@ -6,6 +6,6 @@ public class Runner2 {
 		System.out.println(Runner2.class.getName() + " started");
 		System.out.println("Work in progress...");
 		System.out.println(Runner2.class.getName() + " finished");
-		System.out.println("Hello55f5");
+		System.out.println("Hell2o55sdfsdfsdf5");
 	}
 }
