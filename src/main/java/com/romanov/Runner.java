d39ba3aa826cc26594c6429896b1300f786ec3cb
Igor Romanov <igorromster@gmail.com>
2020-03-28 18:06:10 +0300

    Change even definition function

--- src/main/java/com/romanov/Runner.java
+++ src/main/java/com/romanov/Runner.java
@@ -4,9 +4,13 @@ public class Runner {
 	
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++) {
-			if (i % 2 == 0) {
+			if (isEven(i)) {
 				System.out.println(i);
 			}
 		}
+	}
+	
+	private static boolean isEven(int a) {
+		return a % 2 == 0;
 	}
 }
